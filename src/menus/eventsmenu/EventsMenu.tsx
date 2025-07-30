df1d2306afe6585ba4074cb9d0cb94454941d6b2
Casey Litmer <litmerc@msn.com>
2025-07-30 16:16:25 -0700

    [Add] new even button to event menu

--- src/menus/eventsmenu/EventsMenu.tsx
+++ src/menus/eventsmenu/EventsMenu.tsx
@@ -1,53 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useTheme } from '@mui/material';
 import { useSim } from '../../contexts/SimProvider';
 import Menu from '../../components/menu/Menu';
 import EventItem from './EventItem';
+import AccountEvent from '../../simulation/events/Event';
 import { EventTable } from '../../simulation/types';
 import { addToEventTable, makeEventQueue } from '../../simulation/helpers/eventTableMethods';
 import ScrollContainer from '../../components/menu/ScrollContainer';
-import AccountEvent from '../../simulation/events/Event';
+import MenuItemContainer from '../../components/menu/MenuItemContainer';
+import UtilityButton from '../../components/buttons/UtitlityButton';
+import { useMenu } from '../../contexts/MenuProvider';
+import NewEventMenu from './NewEventMenu';
+import { Add } from '@mui/icons-material';
 
 
 interface EventsMenuProps {
-    accountId?: number  
+  accountId?: number
 }
 
 export default function EventsMenu(props: EventsMenuProps) {
-    const {accountId} = props;
-    const simulation = useSim();
-    const [openState, setOpenState] = useState(false);
+  const { accountId } = props;
+  const simulation = useSim();
+  const { palette } = useTheme();
+  const { openMenu } = useMenu();
+  const [openState, setOpenState] = useState(false);
 
-    //=========================================================================================
-    const eventIds = (accountId === undefined) ?
-        Object.keys(simulation.saveState.events).map(Number) :
-        simulation.saveState.accounts[accountId].eventIds;
+  //=================================================================================
 
-    const eventObjects = Object.values(simulation.simData?.eventsData ?? {})
-        .map(evData => evData.event)
-        .filter(ev => eventIds.includes(ev.id));
+  const ContainerSx = {
+    borderRadius: '4px',
+    backgroundColor: palette.primary.top
+  };
 
-    //Use eventTableMethods to order by time/precedence like in the sim
-    let orderedEvents = {} as EventTable;
-    for (const ev of eventObjects) {
-        orderedEvents = addToEventTable(orderedEvents, ev as AccountEvent);
-    };
-        
-    //Squash list and map back to ids, components...
-    const orderedEventIds = makeEventQueue(orderedEvents).getItems().map((ev) => ev.id);
-    const eventItems = orderedEventIds.map((id) => <EventItem key={id} eventId={Number(id)}/>);
+  //=================================================================================
 
-    //=========================================================================================
-    //Close menu on empty
-    useEffect(() => {
-        if (!eventIds.length) setOpenState(false); 
-    }, [eventIds]);
+  const eventIds = (accountId === undefined) ?
+    Object.keys(simulation.saveState.events).map(Number) :
+    simulation.saveState.accounts[accountId].eventIds;
 
-    //=========================================================================================
-    return (
-        <Menu title='Events' openState={openState} setOpenState={setOpenState}>
-            <ScrollContainer>
-                {eventItems}
-            </ScrollContainer>
-        </Menu>
-    );
+  const eventObjects = Object.values(simulation.simData?.eventsData ?? {})
+    .map(evData => evData.event)
+    .filter(ev => eventIds.includes(ev.id));
+
+  //Use eventTableMethods to order by time/precedence like in the sim
+  let orderedEvents = {} as EventTable;
+  for (const ev of eventObjects) {
+    orderedEvents = addToEventTable(orderedEvents, ev as AccountEvent);
+  };
+
+  //Squash list and map back to ids, components...
+  const orderedEventIds = makeEventQueue(orderedEvents).getItems().map((ev) => ev.id);
+  const eventItems = orderedEventIds.map((id) => <EventItem key={id} eventId={Number(id)} />);
+
+  //=================================================================================
+  //Close menu on empty
+
+  useEffect(() => {
+    if (!eventIds.length) setOpenState(false);
+  }, [eventIds]);
+
+  //=================================================================================
+
+  const handleNewEvent = () => { openMenu(<NewEventMenu accountId={accountId} />) };
+
+  //=================================================================================
+  return (
+    <Menu title='Events' openState={openState} setOpenState={setOpenState}>
+      <ScrollContainer>
+        {accountId !== undefined && <MenuItemContainer sx={ContainerSx}>
+          <UtilityButton
+            name='New Event'
+            icon={Add}
+            handleClick={handleNewEvent}
+          />
+          New Event
+        </MenuItemContainer>}
+        {eventItems}
+      </ScrollContainer>
+    </Menu>
+  );
 };
